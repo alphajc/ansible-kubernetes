9464787feaa395d8c8dbf59c9bf77c9a17a6f3cc
jerry <jerrychan@geekare.com>
2018-08-02 19:08:55 +0800

    添加一个master的k8s集群创建和销毁功能

---
+++ roles/worker/files/worker_setup.sh
@@ -0,0 +1,2 @@
+  kubeadm join 118.178.129.180:6443 --token onxhrq.f26dggjyl8lzfmfr --discovery-token-ca-cert-hash sha256:6adbebe0b85c7c5c62857a0b223ae41d95ca5233be1163cb6292d378d6207b8c --ignore-preflight-errors=cri
+
